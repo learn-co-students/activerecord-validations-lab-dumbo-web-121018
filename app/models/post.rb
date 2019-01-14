945d3d6c9778212c074a22c39397e7a1533f43e3
tarasabella <tarasabella@gmail.com>
2019-01-14 10:11:04 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {mimimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"}
+  # validates :category, inclusion: {in: ['Fiction', 'Non-Fiction'}
 end
